3dee1f75b69431df4421c8a016d5a80361252b68
Thamanmuthappa <thamanmuthappa@gmail.com>
2023-02-01 03:22:07 +0530

    feat: responsive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -14,7 +14,7 @@ function Home() {
         <div className="loader bounce2">
             
             <img src={Logo} alt="Loading..." className='load' />
-            <h1 style={{color:'white',fontSize:'28px', paddingTop:'230px', fontWeight:'700'}}>The Best Date</h1>
+            <h1 style={{color:'white',paddingTop:'230px', fontWeight:'700'}} className="txt">The Best Date</h1>
             
         </div>)
     }
@@ -25,11 +25,11 @@ function Home() {
     }
     return (
         <>
-            <header className="sticky fade-in-down top-0 z-50 grid grid-cols-3 drop-shadow-md p-5 md:px-10" style={{ display: "flex", marginTop:'1%' }}>
+            <header className="sticky fade-in-down top-0 z-50 grid grid-cols-3 drop-shadow-md p-5 md:px-10 " style={{ display: "flex", marginTop:'1%' }}>
                 <a href='/'>
                 <div>
-                    <img src={Logo} alt="Loading..." className='load' style={{height:'60px',width:'60px', marginRight:'300px', marginTop:'-1%'}} />
-                    <span className="text-white" style={{ fontSize:"20px", marginLeft:'64px', fontWeight:'600' }}>The Best Date</span>
+                    <img src={Logo} alt="Loading..." className='load load1' style={{height:'60px',width:'60px', marginRight:'300px', marginTop:'-1%'}} />
+                    <span className="text-white nav" style={{ fontSize:"20px", marginLeft:'64px', fontWeight:'600' }}>The Best Date</span>
                     {/* <button className="btn-hover text-white flex-end font-bold py-2 px-10" style={{ backgroundColor: "#FD9910", borderRadius: "12px", float: "right", fontSize: "16px" }}>
                         <a href="/register">SIGN UP</a>
                     </button> */}
@@ -37,12 +37,12 @@ function Home() {
                 </a>
             </header>
 
-            <div className="grid grid-cols-3 drop-shadow-md p-5 md:px-10 fade-in-left" style={{ display: "flex", marginTop:"15%" }}>
-                <div>
-                    <span className="text-white"style={{ fontSize:"24px", fontWeight:"300"}}>WANT TO CHANGE ROOMS?</span>
-                    <h2 className="text-white grad" style={{ fontSize:"60px",marginTop:"-0.5%" }}>CHANGE YOUR ROOM</h2>
+            <div className="grid grid-cols-3 drop-shadow-md p-5 md:px-10 fade-in-left top" style={{ display: "flex", marginTop:"15%" }}>
+                <div className='bodytxt'>
+                    <span className="text-white sml-txt"style={{ fontSize:"24px", fontWeight:"300"}}>BORED OF DATING APPS?</span>
+                    <h2 className="text-white grad" style={{ fontSize:"60px",marginTop:"-0.5%" }}>YOUR IDEAL MATCH</h2>
                     <h2 className=" grad" style={{ fontSize:"72px",marginTop:"-2%" }}>IN 3 SIMPLE STEPS</h2>
-                    <button className=" text-white btn1 flex-end font-bold py-2 px-10" style={{fontSize: "16px", marginTop:"1%"}} onClick={redirect}>DOWNLOAD</button>
+                    <button className=" text-white btn1 flex-end font-bold py-2 " style={{fontSize: "16px", marginTop:"1%"}} onClick={redirect}>DOWNLOAD</button>
                     <div className=" text-white flex-end font-bold" style={{ borderRadius: "12px", float: "right", fontSize: "16px" }}>
                     </div>
                 </div>
